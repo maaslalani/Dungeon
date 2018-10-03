0f5ccdf10a07970e360f7f408c83a02b85de9bd6
Navin Nayak <navin.nayak1995@gmail.com>
2018-10-03 16:41:19 +0530

    Add Enemy names

--- Enemy.java
+++ Enemy.java
@@ -2,26 +2,38 @@ import java.util.Random;
 
 /**
  * The antagonist whom you must defeat.
- * 
+ *
  * @author Maas Lalani
  * @version 1.2 2016-11-14
  */
-public class Enemy
-{
+public class Enemy {
     /* class fields */
-    /** An array of possible enemy types. */
-    public static final String[] ENEMY_NAMES = { "Zombie", "Skeleton", "Warrior", "Goblin", "Werewolf", "Vampire" };
+    /**
+     * An array of possible enemy types.
+     */
+    public static final String[] ENEMY_NAMES = {"Zombie", "Skeleton", "Warrior", "Goblin", "Werewolf", "Vampire", "Beast Patient", "DarkNut", "Moldorm",
+            "Wallmaster", "Wizrobe", "Beast Claw Hunter", "Hemwick Grave Woman", "Merciless Watcher", "Bone Ash Hunter", "Brick Troll", "Brainsucker",
+            "Snatcher", "Labyrinth Sage", "Chime Maiden", "Executioner", "Mad One", "Silver Lady", "Dusk Walker", "Spark Golem", "Murlocs", "Reapers",
+            "Zinyak", "Spriggans", "Voltrix", "Stray Demon", "Basilisk", "Murkman", "Serpent-man", "Darkwraith"};
 
-    /** The maximum attack damage of this enemy. */
+    /**
+     * The maximum attack damage of this enemy.
+     */
     public static final int MAXIMUM_ATTACK_DAMAGE = 20;
 
-    /** The maximum health of this enemy. */
+    /**
+     * The maximum health of this enemy.
+     */
     public static final int MAXIMUM_HEALTH = 75;
 
-    /** The minimum health of this enemy. */
+    /**
+     * The minimum health of this enemy.
+     */
     public static final int MINIMUM_HEALTH = 1;
 
-    /** The random number generator of this enemy. */
+    /**
+     * The random number generator of this enemy.
+     */
     public static final Random RANDOM = new Random();
 
     /* instance fields */
@@ -31,8 +43,7 @@ public class Enemy
     /**
      * Constructs a new enemy.
      */
-    public Enemy()
-    {
+    public Enemy() {
         /* Fetch a random name from the list of enemies. */
         name = ENEMY_NAMES[RANDOM.nextInt(ENEMY_NAMES.length)];
 
@@ -42,41 +53,37 @@ public class Enemy
 
     /**
      * Returns the damage dealt by this enemy.
-     * 
+     *
      * @return the damage dealt by this enemy
      */
-    public int attack()
-    {
+    public int attack() {
         return RANDOM.nextInt(MAXIMUM_ATTACK_DAMAGE);
     } // end of method attack()
 
     /**
      * Reduces the HP of this enemy by a specifed value.
-     * 
+     *
      * @param damage the amount to reduce the
      */
-    public void takeDamage(int damage)
-    {
+    public void takeDamage(int damage) {
         health = health - damage;
     } // end of method damageDealt(int damage)
 
     /**
      * The name of this enemy.
-     * 
+     *
      * @return the name of the enemy
      */
-    public String name()
-    {
+    public String name() {
         return name;
     } // end of method name()
 
     /**
      * Returns the health of this enemy.
-     * 
+     *
      * @return heatlh of this enemy
      */
-    public int health()
-    {
+    public int health() {
         return health;
     } // end of method health()
 } // end of class Enemy
