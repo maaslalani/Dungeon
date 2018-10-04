ad704081c066b0ee76612df61f3703a5930431be
Jhoe73 <jhoenascimento73@gmail.com>
2018-10-03 22:29:14 -0300

    player does not drink more potion if there is not

--- TheDungeon.java
+++ TheDungeon.java
@@ -148,11 +148,15 @@ public class TheDungeon
                             break;
                         } // end of if (player.health() > player.FULL_HEALTH - player.POTION_HEALING)
                         
+                        if (player.getPotions() < 1) {
+                        	System.out.println("\nYou do not have potions to drink.");
+                        	break;
+                        }
                         player.usePotion();
-    
+                        
                         System.out.println("\nYou drank the potion. Health restored by: " + Player.POTION_HEALING + " HP");
                         System.out.println("Current HP: " + player.health());
-    
+                        
                         delay();
                         break;
 
