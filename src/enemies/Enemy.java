54e2ab19cfef27b6d5dfe51f6967c5b3dba83e74
Elysia Ong <elysia.owx@gmail.com>
2018-10-14 23:16:07 +0800

    Move enemy code into new enemies package

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -1,41 +1,38 @@
+package src.enemies;
+
 import java.util.Random;
 
 /**
  * The antagonist whom you must defeat.
- * 
+ *
  * @author Maas Lalani
  * @version 1.2 2016-11-14
  */
-public class Enemy
+public abstract class Enemy
 {
     /* class fields */
     /** An array of possible enemy types. */
-
     public static final String[] ENEMY_NAMES = {
-      "Zombie",
-      "Skeleton",
-      "Warrior",
-      "Goblin",
-      "Werewolf",
-      "Vampire",
-      "Witch",
-      "Orc",
-      "Imp",
-      "Giant",
-      "Demon",
-      "Spider",
-      "Dragon",
-      "Clown",
-      "Bat",
-      "Undead",
-      "Slime",
-      "Monster",
-      "Anaconda",
-      "Ape",
-      "Snake",
-      "Ghost"
+        // TIER 1
+        "Bat",
+        "Slime",
+        "Snake",
+        "Spider",
+        // TIER 2
+        "Ghost",
+        "Goblin",
+        "Orc",
+        "Skeleton",
+        "Werewolf",
+        // TIER 3
+        "Vampire",
+        "Warrior",
+        "Witch",
+        "Zombie",
+        // TIER 4
+        "Dragon",
+        "Giant"
     };
-
 
     /** The maximum attack damage of this enemy. */
     public static final int MAXIMUM_ATTACK_DAMAGE = 20;
@@ -56,74 +53,77 @@ public class Enemy
     public static final Random RANDOM = new Random();
 
     /* instance fields */
-    private int health;
-    private String name;
+    protected int health;
+    protected String name;
+    protected String type;
 
     /**
      * Constructs a new enemy.
      */
-    public Enemy(Pouch playerPouch)
+    public Enemy()
     {
         /* Fetch a random name from the list of enemies. */
-        name = ENEMY_NAMES[RANDOM.nextInt(ENEMY_NAMES.length)];
+        type = this.getClass().getSimpleName();
+        name = ENEMY_NAMES[RANDOM.nextInt(ENEMY_NAMES.length)] + " " + this.type;
 
-        /* Give the enemy a random health depending on player's coins */
-        if(playerPouch.getCoins() <= 30)
-        {
-            health = RANDOM.nextInt(TIER1_MAXIMUM_HEALTH-TIER1_MINIMUM_HEALTH) + TIER1_MINIMUM_HEALTH;
-        }
-        else if(playerPouch.getCoins() <= 60)
-        {
-            health = RANDOM.nextInt(TIER2_MAXIMUM_HEALTH-TIER2_MINIMUM_HEALTH) + TIER2_MINIMUM_HEALTH;
-        }
-        else if(playerPouch.getCoins() <= 90)
-        {
-            health = RANDOM.nextInt(TIER3_MAXIMUM_HEALTH-TIER3_MINIMUM_HEALTH) + TIER3_MINIMUM_HEALTH;
-        }
-        else
-        {
-            health = RANDOM.nextInt(TIER4_MAXIMUM_HEALTH-TIER4_MINIMUM_HEALTH) + TIER4_MINIMUM_HEALTH;
-        }
-        
     } // end of constructor Enemy()
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     /**
      * Returns the damage dealt by this enemy.
-     * 
+     *
      * @return the damage dealt by this enemy
      */
-    public int attack()
-    {
-        return RANDOM.nextInt(MAXIMUM_ATTACK_DAMAGE);
-    } // end of method attack()
+    public abstract int attack();
+    // end of method attack()
 
     /**
      * Reduces the HP of this enemy by a specifed value.
-     * 
+     *
      * @param damage the amount to reduce the
      */
-    public void takeDamage(int damage)
-    {
-        health = health - damage;
-    } // end of method damageDealt(int damage)
+    public abstract void takeDamage(int damage);
+    // end of method damageDealt(int damage)
 
     /**
      * The name of this enemy.
-     * 
+     *
      * @return the name of the enemy
      */
-    public String name()
+    public String getName()
     {
         return name;
     } // end of method name()
 
     /**
      * Returns the health of this enemy.
-     * 
+     *
      * @return heatlh of this enemy
      */
-    public int health()
+    public int getHealth()
     {
         return health;
     } // end of method health()
+
+    /**
+     * Returns the type of this enemy.
+     *
+     * @return type of this enemy
+     */
+    public String getType()
+    {
+        return type;
+    } // end of method getType()
+
 } // end of class Enemy
